6049f2dfbcc291bbe9be393eef5180e58d178938
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-08 19:31:53 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,10 +1,11 @@
 
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
+  {Sydney:2000, Athens:2004, Beijing:2008, London:2012}
 end
 
 def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  new_hash = create_olympics_hash
 end
 
 def iterate_through_hash
